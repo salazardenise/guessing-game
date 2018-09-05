d5bb0f8e16b858227fedf635e77e3e0e31a5f817
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 12:37:44 -0700

    Validate whether user's guess is not out of range.

--- game.py
+++ game.py
@@ -6,14 +6,20 @@ print ("Howdy, what's your name?")
 name = input ("(type in your name) ")
 
 print ("{}, I'm thinking of a number between 1 and 100.".format(name))
-print ("Try to guess my number")
+print ("Try to guess my number.")
 
 num = randint(1,100)
 tries = 0
 
 while True:
-    tries += 1
+   
     guess = int(input("Your guess? ")) 
+    # check for valid input
+    if guess < 1 or guess > 100:
+        print("You have committed a crime. Please enter a number between 1 and 100")
+        continue
+
+    tries += 1
     if num == guess:
         break
     elif num > guess:
