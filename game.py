cba38a17ba8868208fe88898393352ba29706265
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 12:11:33 -0700

    Greeting the Player

---
+++ game.py
@@ -0,0 +1,10 @@
+"""A number-guessing game."""
+
+# Put your code here
+print ("Howdy, what's your name?")
+name = input ("(type in your name) ")
+
+print ("{}, I'm thinking of a number between 1 and 100.".format(name))
+print ("Try to guess my number")
+
+guess = input("Your guess? ") 
