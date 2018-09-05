1386a2006cc582a72f51620a64819f9deb24fb81
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 16:05:55 -0700

    Limit the Number of Guesses a User Gets to a number the enter at start

--- game.py
+++ game.py
@@ -6,15 +6,20 @@ print ("Howdy, what's your name?")
 name = input ("(type in your name) ")
 playAgain = 'yes'
 bestScore = None
+try:
+    maxTries = int(input("What\'s the maximum number of guesses you need per round? "))
+except ValueError:
+    print("That wasn't a valid number.  You get 20 tries.")
+    maxTries=20
 
 while playAgain == 'yes':
     print ("{}, I'm thinking of a number between 1 and 100.".format(name))
     print ("Try to guess my number.")
 
-    num = randint(1,10)
+    num = randint(1,100)
     tries = 0
 
-    while True:
+    while True and tries < maxTries:
         try:
             guess = int(input("Your guess? ")) 
         except ValueError: 
@@ -34,12 +39,18 @@ while playAgain == 'yes':
         else:
             print ("Your guess in too high, try again.")
 
-    print ("Well done, {}! You found my number in {} tries!".format(name, tries))
-    if bestScore is None or tries < bestScore:
-        bestScore = tries
-    # print ("bestScore so far is {}".format(bestScore))
+    if tries >= maxTries:
+        print("Too many tries.  The number was {}.   :-( ".format(num))
+    else:
+        print ("Well done, {}! You found my number in {} tries!".format(name, tries))
+        if bestScore is None or tries < bestScore:
+            bestScore = tries
+        # print ("bestScore so far is {}".format(bestScore))
 
     playAgain = input("Would you like to play again? (enter Yes to play again):  ")
     playAgain = playAgain.lower()
 
-print ("Thanks for playing! Your best score today was {} guesses. Bye.".format(bestScore))
+if bestScore is None:
+    print ("Thanks for playing! You didn't win any games today.")
+else: 
+    print ("Thanks for playing! Your best score today was {} guesses. Bye.".format(bestScore))
